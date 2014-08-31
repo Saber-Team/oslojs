8556e3211c238279c4085c5ebd69d7657180126f
AceMood <zmike86@163.com>
2014-08-31 17:29:05 +0800

    rebuild

--- build/sogou.js
+++ build/sogou.js
@@ -1,5 +1,5 @@
 /** 
  * Sogou JavaScript Library 
- * 2014-08-29 build 
+ * 2014-08-31 build 
  */
 !function(a,b){"use strict";var c=Object.prototype.toString;function d(b){for(var c,d=b.split("."),e=a;d.length&&(c=d.shift());){if(!e[c])throw e+"'s "+c+" do not exist";e=e[c]}return e}function e(a){var b=[];if("[object String]"===c.call(a))b.push(d(a));else if("[object Array]"===c.call(a))for(var e=0;e<a.length;++e)b.push(d(a[e]));return b}function f(c,d){var e=c.split("."),f=a;e[0]in f||!f.execScript||f.execScript("var "+e[0]);for(var g;e.length&&(g=e.shift());)e.length||d===b?f=f[g]?f[g]:f[g]={}:f[g]=d}a.sogou="undefined"!=typeof module&&module.exports?function(a,b,c){module.exports=c()}:"function"==typeof a.define&&a.define.amd?function(a,b,c){define(a,b,c)}:function(b,c,d){var g=e(c);f(b,d.apply(a,g))}}(this);
