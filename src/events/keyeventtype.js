0a853f81f24cda73ec833b4883aef4725e1ed9a0
AceMood <zmike86@163.com>
2014-09-24 22:33:38 +0800

    add key event type module

---
+++ src/events/keyeventtype.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview KeyEvent事件类型
+ * @modified Leo.Zhang
+ * @email zmike86@gmail.com
+ */
+
+sogou('Sogou.Events.KeyEventType',
+    [],
+    function() {
+
+        'use strict';
+
+        /**
+         * 事件类型
+         * @enum {string}
+         */
+        return {
+            KEY: 'key'
+        };
+    }
+);
