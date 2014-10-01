3398139ef174e5e6108943947b277e0fdb025aa2
AceMood <zmike86@163.com>
2014-10-01 12:33:14 +0800

    add gitignore file

---
+++ src/net/eventtype.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview network类通用的一些事件.
+ * @modified Leo.Zhang
+ * @email zmike86@gmail.com
+ */
+
+define('Sogou.Net.EventType', [], function() {
+
+    'use strict';
+
+    /**
+     * network事件. 里面也包含一些语义化的自定义事件, 作为整个事件系统的通用机制.
+     * @enum {string}
+     */
+    return {
+        COMPLETE: 'complete',
+        SUCCESS: 'success',
+        ERROR: 'error',
+        ABORT: 'abort',
+        READY: 'ready', // 这个表示前一个动作已完成可投入使用
+        READY_STATE_CHANGE: 'readystatechange',
+        TIMEOUT: 'timeout', //xhr2支持
+        INCREMENTAL_DATA: 'incrementaldata', // 这个应该是在web－stream技术中得到的事件
+        PROGRESS: 'progress' // 详见https://developer.mozilla.org/en-US/docs/Web/API/ProgressEvent
+    };
+});
