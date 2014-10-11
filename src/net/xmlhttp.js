ea9661207cc75f10d3076a544cbd30b233706e21
AceMood <zmike86@163.com>
2014-10-11 23:50:17 +0800

    add net xmlhttp modules2codebase

---
+++ src/net/xmlhttp.js
@@ -0,0 +1,87 @@
+/**
+ * @fileoverview 底层的处理XMLHttpRequest的操作模块.
+ * @modified Leo.Zhang
+ * @email zmike86@gmail.com
+ */
+
+define('Sogou.Net.XmlHttp',
+    ['Sogou.Net.DefaultXmlHttpFactory'],
+    function(DefaultXmlHttpFactory) {
+
+        'use strict';
+
+        /**
+         * 创建 XMLHttpRequest 对象的静态类.
+         * @return {!(XMLHttpRequest|GearsHttpRequest)} A new XMLHttpRequest object.
+         */
+        var XmlHttp = function() {
+            return XmlHttp.factory_.createInstance();
+        };
+
+
+        /**
+         * 用这个静态方法获得XMLHttpRequest objects的配置对象.
+         * @return {Object} The options.
+         */
+        XmlHttp.getOptions = function() {
+            return XmlHttp.factory_.getOptions();
+        };
+
+
+        /**
+         * XmlHttp对象中可以含有的选项.
+         * @enum {number}
+         */
+        XmlHttp.OptionType = {
+            /**
+             * 是否用util.nullFunction 设置 onreadystatechange 而不是 null.
+             */
+            USE_NULL_FUNCTION: 0,
+            /**
+             * NOTE(user): 在IE中如果send()发送到本地(*local* request)时会出错,则readystate
+             * 会一直是COMPLETE. 我们得忽略这种情况并且在send()外包裹try/catch 才能捕捉到这个error.
+             */
+            LOCAL_REQUEST_ERROR: 1
+        };
+
+
+        /**
+         * XMLHTTP Status常量, 见:
+         * http://msdn.microsoft.com/library/default.asp?url=/library/
+         *   en-us/xmlsdk/html/0e6a34e4-f90c-489d-acff-cb44242fafc6.asp
+         * @enum {number}
+         */
+        XmlHttp.ReadyState = {
+            UNINITIALIZED: 0,
+            LOADING: 1,
+            LOADED: 2,
+            INTERACTIVE: 3,
+            COMPLETE: 4
+        };
+
+
+        /**
+         * 一个静态的私有属性,保存了创建XMLHttpRequest对象的工厂实例,是全局的工厂.
+         * @type {XmlHttpFactory}
+         * @private
+         */
+        XmlHttp.factory_;
+
+
+        /**
+         * 设置XmlHttp的factory_对象.
+         * @param {!XmlHttpFactory} factory New global factory object.
+         */
+        XmlHttp.setGlobalFactory = function(factory) {
+            XmlHttp.factory_ = factory;
+        };
+
+
+        // Set the global factory to an instance of the default factory.
+        XmlHttp.setGlobalFactory(new DefaultXmlHttpFactory());
+
+
+        return XmlHttp;
+
+    }
+);
---
+++ src/net/xmlhttpfactory.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview 一个接口. 创建XMLHttpRequest对象和它们元数据(metadata)的抽象工厂.
+ * @modified Leo.Zhang
+ * @email zmike86@gmail.com
+ */
+
+define('Sogou.Net.XmlHttpFactory',
+    ['Sogou.Util'],
+    function(util) {
+
+        'use strict';
+
+        /**
+         * XmlHttpRequest 的抽象工厂类.
+         * @constructor
+         */
+        var XmlHttpFactory = function() {};
+
+
+        /**
+         * XHR选项 - 只调用internalGetOptions一次.
+         * @type {Object}
+         * @private
+         */
+        XmlHttpFactory.prototype.cachedOptions_ = null;
+
+
+        /**
+         * @return {!(XMLHttpRequest|GearsHttpRequest)} 返回XMLHttpRequest实例.
+         */
+        XmlHttpFactory.prototype.createInstance = util.abstractMethod;
+
+
+        /**
+         * @return {Object} 返回创建时的配置对象.
+         */
+        XmlHttpFactory.prototype.getOptions = function() {
+            return this.cachedOptions_ ||
+                (this.cachedOptions_ = this.internalGetOptions());
+        };
+
+
+        /**
+         * 一个受保护方法. 在子类中重写可以保全getOptions()返回的选项.
+         * 这个方法不会被调用, 看DefaultXmlHttpFactory的实现就知道了.
+         * @return {Object} 返回创建xhr对象的配置对象.
+         * @protected
+         */
+        XmlHttpFactory.prototype.internalGetOptions = util.abstractMethod;
+
+
+        return XmlHttpFactory;
+
+    }
+);
