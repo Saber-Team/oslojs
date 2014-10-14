73f6e9d763caf33ffb12165b1f4862fc68b8e965
AceMood <zmike86@163.com>
2014-10-14 19:56:13 +0800

    rebuild

--- build/net/xmlhttp.js
+++ build/net/xmlhttp.js
@@ -2,4 +2,4 @@
  * Sogou JavaScript Library 
  * 2014-10-14 build 
  */
-define("Sogou.Net.XmlHttp",["Sogou.Net.DefaultXmlHttpFactory"],function(a){"use strict";var b=function(){return b.factory_.createInstance()};return b.getOptions=function(){return b.factory_.getOptions()},b.OptionType={USE_NULL_FUNCTION:0,LOCAL_REQUEST_ERROR:1},b.ReadyState={UNINITIALIZED:0,LOADING:1,LOADED:2,INTERACTIVE:3,COMPLETE:4},b.factory_,b.setGlobalFactory=function(a){b.factory_=a},b.setGlobalFactory(new a),b});
+define("Sogou.Net.XmlHttp",["Sogou.Net.DefaultXmlHttpFactory"],function(a){"use strict";var b=null,c=function(){return b.createInstance()};return c.getOptions=function(){return b.getOptions()},c.OptionType={USE_NULL_FUNCTION:0,LOCAL_REQUEST_ERROR:1},c.ReadyState={UNINITIALIZED:0,LOADING:1,LOADED:2,INTERACTIVE:3,COMPLETE:4},c.setGlobalFactory=function(a){b=a},c.setGlobalFactory(new a),c});
