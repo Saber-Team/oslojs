276f37ef751015391796dc9ce3bd644d1c85ef32
AceMood <zmike86@163.com>
2014-11-05 22:08:33 +0800

    modify testcases

--- src/util/memoize.js
+++ src/util/memoize.js
@@ -9,10 +9,14 @@ define('@memo', ['@util'], function(util) {
 
     'use strict';
 
+
+    var memo = {};
+
+
     /**
      * @define {boolean} 单元测试中也开启memoize.
      */
-    var ENABLE_MEMOIZE = true;
+    memo.ENABLE_MEMOIZE = true;
 
 
     /**
@@ -39,42 +43,43 @@ define('@memo', ['@util'], function(util) {
     }
 
 
-    // export
-    return {
-        /**
-         * 一个包裹器装饰一个函数缓存他的执行结果. 但是调用时对于引用参数不能准确序列化,见simpleSerializer.
-         * @param {Function} f 记忆函数. 函数返回值可能只依赖于他的形参和上下文..
-         * @param {function(number, Object): string=} opt_serializer 自定义的序列化函数.
-         * @this {Object} 函数上下文或拥有者.
-         * @return {!Function} 返回闭包.
-         */
-        memoize: function(f, opt_serializer) {
-            var functionUid = util.getUid(f);
-            var serializer = opt_serializer || simpleSerializer;
-
-            return function() {
-                if (ENABLE_MEMOIZE) {
-                    // 严格模式下, 函数在全局执行时上下文 'this' 是undefined. See:
-                    // https://developer.mozilla.org/en/JavaScript/Strict_mode
-                    var thisOrGlobal = this || util.global;
-                    // Maps the serialized list of args to the corresponding return value.
-                    var cache = thisOrGlobal[CACHE_PROPERTY_] || (thisOrGlobal[CACHE_PROPERTY_] = {});
-                    var key = serializer(functionUid, arguments);
-                    // 返回函数执行结果
-                    return cache.hasOwnProperty(key) ? cache[key] : (cache[key] = f.apply(this, arguments));
-                } else {
-                    return f.apply(this, arguments);
-                }
-            };
-        },
-
-        /**
-         * 清空缓存.
-         * @param {Object} cacheOwner 记忆函数的拥有者或上下文.
-         */
-        clearCache: function(cacheOwner) {
-            cacheOwner[CACHE_PROPERTY_] = {};
-        }
+    /**
+     * 清空缓存.
+     * @param {Object} cacheOwner 记忆函数的拥有者或上下文.
+     */
+    memo.clearCache =function(cacheOwner) {
+        cacheOwner[CACHE_PROPERTY_] = {};
     };
+
+
+    /**
+     * 一个包裹器装饰一个函数缓存他的执行结果. 但是调用时对于引用参数不能准确序列化,见simpleSerializer.
+     * @param {Function} f 记忆函数. 函数返回值可能只依赖于他的形参和上下文..
+     * @param {function(number, Object): string=} opt_serializer 自定义的序列化函数.
+     * @this {Object} 函数上下文或拥有者.
+     * @return {!Function} 返回闭包.
+     */
+    memo.memoize = function(f, opt_serializer) {
+        var functionUid = util.getUid(f);
+        var serializer = opt_serializer || simpleSerializer;
+
+        return function() {
+            if (memo.ENABLE_MEMOIZE) {
+                // 严格模式下, 函数在全局执行时上下文 'this' 是undefined. See:
+                // https://developer.mozilla.org/en/JavaScript/Strict_mode
+                var thisOrGlobal = this || util.global;
+                // Maps the serialized list of args to the corresponding return value.
+                var cache = thisOrGlobal[CACHE_PROPERTY_] || (thisOrGlobal[CACHE_PROPERTY_] = {});
+                var key = serializer(functionUid, arguments);
+                // 返回函数执行结果
+                return cache.hasOwnProperty(key) ? cache[key] : (cache[key] = f.apply(this, arguments));
+            } else {
+                return f.apply(this, arguments);
+            }
+        };
+    };
+
+    // export
+    return memo;
 
 });
