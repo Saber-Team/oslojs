1a115c050bd459d3b0183b76f7ce216381cf5712
AceMood <zmike86@163.com>
2014-11-17 23:07:55 +0800

    rebuild

--- build/fx/fadein.js
+++ build/fx/fadein.js
@@ -1,2 +1,2 @@
 /** Oslo JavaScript Framework. */
-define("@fx.fadeIn",["@util","@fx.fade"],function(a,b){"use strict";var c=function(a,c,d){b.call(this,a,0,1,c,d)};return a.inherits(c,b),c});
+define("@fx.fadeIn",["@util","@fx.Fade"],function(a,b){"use strict";var c=function(a,c,d){b.call(this,a,0,1,c,d)};return a.inherits(c,b),c});
