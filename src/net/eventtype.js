847d37d168fc72ebea6dddf524b6bbaf8853976e
AceMood <zmike86@163.com>
2014-11-24 18:10:23 +0800

    directly return object

--- src/net/eventtype.js
+++ src/net/eventtype.js
@@ -4,23 +4,18 @@
  * @email zmike86@gmail.com
  */
 
-define('@net.eventType', [], function() {
-
-    'use strict';
-
-    /**
-     * network事件. 里面也包含一些语义化的自定义事件, 作为整个事件系统的通用机制.
-     * @enum {string}
-     */
-    return {
-        COMPLETE: 'complete',
-        SUCCESS: 'success',
-        ERROR: 'error',
-        ABORT: 'abort',
-        READY: 'ready', // 这个表示前一个动作已完成可投入使用
-        READY_STATE_CHANGE: 'readystatechange',
-        TIMEOUT: 'timeout', //xhr2支持
-        INCREMENTAL_DATA: 'incrementaldata', // 这个应该是在web－stream技术中得到的事件
-        PROGRESS: 'progress' // 详见https://developer.mozilla.org/en-US/docs/Web/API/ProgressEvent
-    };
+/**
+ * network事件. 里面也包含一些语义化的自定义事件, 作为整个事件系统的通用机制.
+ * @enum {string}
+ */
+define('@net.eventType', [], {
+    COMPLETE: 'complete',
+    SUCCESS: 'success',
+    ERROR: 'error',
+    ABORT: 'abort',
+    READY: 'ready', // 这个表示前一个动作已完成可投入使用
+    READY_STATE_CHANGE: 'readystatechange',
+    TIMEOUT: 'timeout', //xhr2支持
+    INCREMENTAL_DATA: 'incrementaldata', // 这个应该是在web－stream技术中得到的事件
+    PROGRESS: 'progress' // 详见https://developer.mozilla.org/en-US/docs/Web/API/ProgressEvent
 });
